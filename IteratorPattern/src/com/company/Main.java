ffdbf8e14562af8270928cf51c541fd9ae75943f
陈晓波 <1171856045@qq.com>
2018-11-05 23:44:29 +0800

    完成书本练习

--- IteratorPattern/src/com/company/Main.java
+++ IteratorPattern/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        BookShelf bookShelf = new BookShelf(4);
+        BookShelf bookShelf = new BookShelf();
         bookShelf.appendBook(new Book("网络是怎样连接的"));
         bookShelf.appendBook(new Book("图解设计模式"));
         bookShelf.appendBook(new Book("算法新解"));
